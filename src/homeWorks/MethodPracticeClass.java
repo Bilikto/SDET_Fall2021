c4a2c1239a9cafc5c94e94ab4cb257bb5aad8550
Bilikto <dashievbilikto@gmail.com>
2021-11-04 10:07:38 -0500

    more practice using methods

--- src/homeWorks/MethodPracticeClass.java
+++ src/homeWorks/MethodPracticeClass.java
@@ -3,10 +3,28 @@ package homeWorks;
 import java.util.Scanner;
 
 public class MethodPracticeClass {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         printName("Billy");
-        new MethodPracticeClass().printAgeMinusTen(34);
+        MethodPracticeClass myObj = new MethodPracticeClass();
+        myObj.printAgeMinusTen(34);
         printQuote();
+
+        myObj.printMyFavoriteQuote("Being happy is the greatest form of success");
+
+        System.out.println("Enter DOB:");
+        String dob = scanner.nextLine();
+        System.out.println("First name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Last name:");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter your height");
+        int height = scanner.nextInt();
+        System.out.println("Enter your weight");
+        int weight = scanner.nextInt();
+        System.out.println(recruitingToAirForce(dob, firstName, lastName, height, weight));
+
 
     }
 
@@ -19,11 +37,24 @@ public class MethodPracticeClass {
     }
 
     private static void printQuote() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your favorite quote...");
         String quote = scanner.nextLine();
-        System.out.println("My favorite quote is: " + "\"" + quote + "\"");
+        System.out.println("Your favorite quote is: " + "\"" + quote + "\"");
     }
 
+    private void printMyFavoriteQuote(String str) {
+        System.out.println("My favorite quote is "+ str);
+    }
+
+    private static String recruitingToAirForce(String dob, String firstName, String lastName, int height, int weight) {
+        return (height > 160 && weight < 220) ? "Congratulations! You qualify for United States Air Force!" : "Sorry you don’t qualify..";
+
+//        if (height > 160 && weight < 220) {
+//            System.out.println("Congratulations! You qualify for United States Air Force!");
+//        } else {
+//            System.out.println("Sorry you don’t qualify..");
+//        }
+    }
 
 }
+git add .
