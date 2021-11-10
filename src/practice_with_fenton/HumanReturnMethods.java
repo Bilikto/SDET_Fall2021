5ece62fd44e79e6974064f5c68ee072e9738ddf2
Bilikto <dashievbilikto@gmail.com>
2021-11-09 18:01:35 -0600

    Practice methods

---
+++ src/practice_with_fenton/HumanReturnMethods.java
@@ -0,0 +1,49 @@
+package practice_with_fenton;
+
+import java.util.Scanner;
+
+public class HumanReturnMethods {
+
+    public static void main(String[] args) {
+
+//        printName();
+
+//        Human billy = new Human();
+//        billy.printAge();
+
+//        new Human().printAge();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name..");
+        String name = scanner.nextLine();
+        System.out.println("Enter age..");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter last name");
+        String lastName = scanner.nextLine();
+
+        printName(name);
+
+        HumanReturnMethods man = new HumanReturnMethods();
+        man.printAge(age);
+        String returnedLastName = man.getLastName(lastName);
+        System.out.println(returnedLastName);
+
+
+    }
+
+    public static void printName(String name) {
+        System.out.println(name);
+    }
+
+    public void printAge(int age) {
+        System.out.println(age);
+    }
+
+    public String getLastName(String lastName) {
+        return lastName;
+    }
+
+
+
+}
