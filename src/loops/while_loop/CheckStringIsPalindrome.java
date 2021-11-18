bfbe0da067c667828bef78bf1ea228b5ac6c5f2f
Bilikto <dashievbilikto@gmail.com>
2021-11-17 21:11:12 -0600

    Palindrome

---
+++ src/loops/while_loop/CheckStringIsPalindrome.java
@@ -0,0 +1,43 @@
+package loops.while_loop;
+
+import java.util.Scanner;
+
+public class CheckStringIsPalindrome {
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "madam";
+
+        System.out.print("String 1 ");
+        if (isPalindrome(str1)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        System.out.println();
+
+        System.out.print("String 2 ");
+        if (isPalindrome(str2)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length()-1;
+
+        while(i <= j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
