ec6e2e3b33dc68dfd2540127ac29a67be301d90d
Bilikto Dashiev <dashievbilikto@gmail.com>
2021-12-16 14:08:48 -0600

    ArrayList add(), get(), Collections.sort() methods practice

--- src/practice_with_Fenton/ArrayListClass.java
+++ src/practice_with_Fenton/ArrayListClass.java
@@ -1,13 +1,16 @@
 package practice_with_Fenton;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ArrayListClass {
 
     public static void main(String[] args) {
+
+        //Boxing --> converting primitive to Wrapper Object manually
+        //Autoboxing --> converting primitive to Wrapper Object type automatically java handles it
+
+        //Unboxing --> converting Wrapper object to primitive type
+        //AutoUnboxing --> converting Wrapper object to primitive
 
         /*
         byte -- >  Byte
@@ -30,9 +33,9 @@ public class ArrayListClass {
 //        Integer wrapperInt2 = Integer.valueOf(numStr);
 //        System.out.println(wrapperInt2);
 
-        int[] arrNums = {1, 5, 88, 98, 101};
-        ArrayList<int[]> listNums = new ArrayList<>(Arrays.asList(arrNums));
-        System.out.println(Arrays.toString(listNums.get(0)));
+//        int[] arrNums = {1, 5, 88, 98, 101};
+//        ArrayList<int[]> listNums = new ArrayList<>(Arrays.asList(arrNums));
+//        System.out.println(Arrays.toString(listNums.get(0)));
 
 
 //        List<Integer> arrN = new ArrayList<Integer>(Arrays.asList(arrNums));
@@ -46,6 +49,33 @@ public class ArrayListClass {
 //        String[] str = {"New York", "Washington", "Baltimore"};
 //        ArrayList<String> strList = new ArrayList<>(Arrays.asList(str));
 //        System.out.println(strList);
+
+    //===============================================================================================================
+
+        /*Methods:
+        add();
+        get();
+
+
+         */
+      /*  int[] arrNums1 = {1, 5, 88, 98, 101};
+        int[] arrNums2 = {2, 8, 108, 99, 11};
+        List<int[]> scoreList = new ArrayList<>(Arrays.asList(arrNums1));
+        scoreList.add(arrNums2);
+        System.out.println(Arrays.toString(scoreList.get(0)));
+        System.out.println(Arrays.toString(scoreList.get(1)));*/
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(80);
+        nums.add(55);
+        nums.add(90);
+        nums.add(35);
+        System.out.println(nums.get(1));
+
+
+        //Collections.sort()
+        Collections.sort(nums);
+        System.out.println(nums);
 
 
 
