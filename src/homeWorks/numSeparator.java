eaaca653ebccfd736ed25803dfc989d95cacd69f
Bilikto Dashiev <dashievbilikto@gmail.com>
2021-12-21 12:49:04 -0600

    Homework num separator

--- src/homeWorks/numSeparator.java
+++ src/homeWorks/numSeparator.java
@@ -1,12 +1,13 @@
 package homeWorks;
 
-import java.util.ArrayList;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class numSeparator {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter at least 5 numbers:");
         int nums = scanner.nextInt();
@@ -15,5 +16,15 @@ public class numSeparator {
         char[] digits = number.toCharArray();
         System.out.println(Arrays.toString(digits));
 
+//        String[] digits2 = number.split("(?<=.)");
+//        System.out.println(Arrays.toString(digits2));
+
     }
+
+
+
+
+
 }
+
+
