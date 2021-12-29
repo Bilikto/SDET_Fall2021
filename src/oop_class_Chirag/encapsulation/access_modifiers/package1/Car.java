deafa8b5d6b28c6da5102be819ea00fd68d5ed8d
Bilikto Dashiev <dashievbilikto@gmail.com>
2021-12-29 10:45:24 -0600

    Access modifiers

---
+++ src/oop_class_Chirag/encapsulation/access_modifiers/package1/Car.java
@@ -0,0 +1,10 @@
+package oop_class_Chirag.encapsulation.access_modifiers.package1;
+
+public class Car {
+    public Car(){
+        System.out.println("1. Starting the car"); // start the car
+    }
+    public void drive(){
+        System.out.println("2. Driving"); // driving
+    }
+}
---
+++ src/oop_class_Chirag/encapsulation/access_modifiers/package2/AccessModifierTest.java
@@ -0,0 +1,10 @@
+package oop_class_Chirag.encapsulation.access_modifiers.package2;
+
+import oop_class_Chirag.encapsulation.access_modifiers.package1.Car;
+
+public class AccessModifierTest {
+    public static void main(String []args){
+        Car c = new Car();
+        c.drive();
+    }
+}
