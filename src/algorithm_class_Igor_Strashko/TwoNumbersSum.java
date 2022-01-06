2513d10a365bfb6a254dce079d7ba772d3331ab2
Bilikto <dashievbilikto@gmail.com>
2022-01-06 16:36:48 -0600

    OOP concepts

---
+++ src/algorithm_class_Igor_Strashko/TwoNumbersSum.java
@@ -0,0 +1,23 @@
+package algorithm_class_Igor_Strashko;
+
+import java.util.Arrays;
+
+public class TwoNumbersSum {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 5, -4, 8, 11, 1, -1, 6};
+        System.out.println(Arrays.toString(getTwoNumberSum(nums, 10)));
+    }
+
+    public static int[] getTwoNumberSum(int[] arr, int target) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i+1; j < arr.length; j++) {
+                int sum = arr[j] + arr[i];
+                if(sum == target) {
+                    return new int[] {arr[i], arr[j]};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
